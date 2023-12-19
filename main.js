7c26927ad042152a961fc3f56cbc2509d46d2d67
Eduarda Nunes <eduarda.nunes@estudante.ufjf.br>
2023-12-19 00:47:32 -0300

    correções main.js

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 import { getEstoque, limpaEstoque, transacaoNoEstoque } from "./estoque.js"
 
-document.entrada.addEventListener('submit', leFormulario);
+document.entrada.addEventListener('submit', leFormulario)
 
-const olJoao = document.querySelector('#olJoao');
-const olMaria = document.querySelector('#olMaria');
+
+const olJoao = document.querySelector("#joao")
+const olMaria = document.querySelector("#maria")
+
+atualizaTela()
 
 function leFormulario(event){
-    event.preventDefault();
-
+    event.preventDefault()
+    
     const fruta = document.entrada.fruta.value
     const quantidade = document.entrada.quantidade.valueAsNumber
     const origem = document.entrada.origem.value
     const destino = document.entrada.destino.value
+    
+    console.log(`solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`)
 
     transacaoNoEstoque(origem, destino, fruta, quantidade)
     atualizaTela()
-
-    console.log(`fruta: ${fruta} qtd: ${quantidade}`);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
